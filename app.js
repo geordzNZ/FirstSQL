a0d5e149f1c3a9338eb80fd2754fa13418b4f496
Geordie King <geordie.king@gmail.com>
2022-02-04 18:48:31 +0000

    first add of sequelize

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 console.log('hi from main.js')
-
 require('dotenv').config()
-const PORT = 4242
-
+const { Sequelize } = require('sequelize');
 console.log((process.env.SECRET_KEY))
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize('GEORDIESHP', 'username','password' {
+  host: 'localhost',
+  dialect: 'mssql'
+});
+
+
+try {
+    sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
